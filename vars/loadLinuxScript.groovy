01bf3969297ddb6f374696ee7ce6f01778dd7f01
SAIKUMAR SRIGIRIRAJU <80637313+SSRIGIRIRAJU22@users.noreply.github.com>
2023-02-23 13:57:20 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "scripts/generate_pesh_passwords.py"    
-  writeFile file: "generate_pesh_passwords.py", text: scriptcontents 
-  sh "chmod +x generate_pesh_passwords.py"
+  //def scriptcontents = libraryResource "scripts/generate_pesh_passwords.py"
+  def scriptcontents = libraryResource "scripts/hello-world.py"
+  writeFile file: "hello-world.py", text: scriptcontents 
+  sh "chmod +x hello-world.py"
 } 
