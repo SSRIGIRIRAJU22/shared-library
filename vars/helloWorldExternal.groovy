fd69e7726730f6c8c5ad5d7ec61ebd7545ccf6a0
SAIKUMAR SRIGIRIRAJU <80637313+SSRIGIRIRAJU22@users.noreply.github.com>
2023-02-23 23:34:35 +0530

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,9 +1,4 @@
 def call(Map config = [:]) {
-    //loadLinuxScript(name: 'generate_pesh_passwords.py')
-    //loadLinuxScript(name: 'hello-world.py')
-    loadLinuxScript(name: 'hello-world.sh')
-    //sh "./generate_pesh_passwords.py -u ${config.username} -p ${config.password} --hmc ${config.HMCs} -r ${config.rtype} -f"
-    sh './hello-world.sh'
-    sh 'which bash'
-    sh 'which python'
+    loadLinuxScript(name: 'generate_pesh_passwords.py')
+    sh "./generate_pesh_passwords.py -u ${config.username} -p ${config.password} --hmc ${config.HMCs} -r ${config.rtype} -f"
 }
